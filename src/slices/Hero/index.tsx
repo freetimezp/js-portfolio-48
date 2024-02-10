7c11ffa5f7969db44b0a04588107e551ac7afea4
freetimezp <kraftukrnet@ukr.net>
2024-02-10 20:42:39 +0200

    05. js-portfolio-48. Shapes animation use Three js.

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -2,9 +2,10 @@
 import { useEffect, useRef } from "react";
 import { Content, KeyTextField } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
-import gsap from 'gsap';
+import { gsap } from 'gsap';
 
 import Bounded from "@/components/Bounded";
+import Shapes from "./Shapes";
 
 export type HeroProps = SliceComponentProps<Content.HeroSlice>;
 
@@ -36,7 +37,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
 
       tl.fromTo('.job-title',
         {
-          y: 20, opacity: 0, scale: 1.5
+          y: 20, opacity: 0, scale: 1.3
         },
         {
           y: 0,
@@ -69,6 +70,8 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       ref={component}
     >
       <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
+        <Shapes />
+
         <div className="col-start-1 md:row-start-1">
           <h1
             className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none
