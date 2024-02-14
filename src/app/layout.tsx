4223c07a635b14ab53de2c339a8f59cc297f46fc
freetimezp <kraftukrnet@ukr.net>
2024-02-14 16:35:03 +0200

    13. js-portfolio-48. bg shine effect.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
+import clsx from "clsx";
 import "./globals.css";
 
 const urbanist = Urbanist({ subsets: ["latin"] });
@@ -19,10 +20,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="bg-slate-900 text-slate-100">
-      <body className={urbanist.className} suppressHydrationWarning={true}>
+      <body className={clsx(urbanist.className, "relative min-h-screen")} suppressHydrationWarning={true}>
         <Header />
         {children}
         <Footer />
+        <div className="absolute inset-0 -z-50 max-h-screen background-gradient"></div>
+        <div className="absolute pointer-events-none inset-0 -z-40 h-full bg-[url('/noisetexture.jpg')] 
+          opacity-20 mix-blend-soft-light"></div>
       </body>
     </html>
   );
